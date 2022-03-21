7442745b8c89eb53bb8352a7b014a1c1c49b372c
Igor Kuratov <kuratov.ia@dclouds.ru>
2022-03-21 11:11:49 +0000

    DSCTUD-12: Header and footer

---
+++ front/src/components/layout/FooterComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="footer">
+    <div class="footer__contacts">
+      <a href="#">
+        <icon-template
+          width="40"
+          height="40"
+          name="telegram"
+          class="footer__contacts-item"
+        />
+      </a>
+      <a href="#">
+        <icon-template
+          width="40"
+          height="40"
+          name="whatsapp"
+          class="footer__contacts-item"
+        />
+      </a>
+      <a href="#">
+        <icon-template
+          width="40"
+          height="40"
+          name="instagram"
+          class="footer__contacts-item"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  justify-content: right;
+  height: 120px;
+  background: url('../../assets/images/footer-image.jpg') 0 0 no-repeat;
+
+  &__contacts {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 200px;
+
+    &-item {
+      margin-top: 50px;
+    }
+  }
+}
+</style>
---
+++ front/src/layout/Layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="body">
+    <header-component />
+    <div class="body__center">
+      <router-view />
+    </div>
+    <footer-component />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HeaderComponent from '@/components/layout/HeaderComponent.vue'
+import FooterComponent from '@/components/layout/FooterComponent.vue'
+
+export default defineComponent({
+  name: 'Layout',
+  components: { HeaderComponent, FooterComponent },
+})
+</script>
+
+<style lang="scss" scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  &__center {
+    flex: 1 1 auto;
+  }
+}
+</style>
