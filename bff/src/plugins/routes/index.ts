9a14cdc5b81e2e3da008006874e076eb92fbdce1
Kuratov Igor <kuratov.ia@dclouds.ru>
2022-03-30 12:14:17 +0700

    DSCTUD-3: get ability info

--- bff/src/plugins/routes/index.ts
+++ bff/src/plugins/routes/index.ts
@@ -1,1 +1,2 @@
 export { default as pokemonListRoute } from './pokemon/pokemonListingRoute'
+export { default as abilityRoute } from './pokemon/abilityRoute'
---
+++ bff/src/plugins/routes/pokemon/abilityRoute.ts
@@ -0,0 +1,52 @@
+import { Static, Type } from '@sinclair/typebox'
+import { FastifyInstance } from 'fastify/types/instance'
+import { AbilityMapper } from '../../mappers/abilityMapper'
+
+const AbilitySchema = Type.Object({
+  name: Type.String(),
+  description: Type.String(),
+})
+
+const ResponseSchema = Type.Object({
+  ability: AbilitySchema,
+})
+export type ResponseSchemaType = Static<typeof ResponseSchema>
+
+const ParamsSchema = Type.Object({
+  abilityName: Type.Optional(Type.String())
+})
+
+const abilityRoute = (fastify: FastifyInstance) => {
+  return fastify.get<{
+    Response: ResponseSchemaType,
+    Params: Static<typeof ParamsSchema>
+  }>(
+    '/api/ability/:abilityName',
+    {
+      schema: {
+        params: ParamsSchema,
+        response: {
+          200: ResponseSchema,
+        },
+      }
+    },
+    async (req, repl) => {
+      try {
+        const abilityName = req.params.abilityName
+        if (abilityName) {
+          const abilityFromApi = await fastify.axios.get(`https://pokeapi.co/api/v2/ability/${abilityName}`)
+
+          const abilityInfo = AbilityMapper.mapAbilityInfoToFrontend(abilityFromApi.data)
+
+          await repl.send({
+            ability: abilityInfo
+          })
+        }
+      } catch (e) {
+        fastify.log.error(e)
+      }
+    }
+  )
+}
+
+export default abilityRoute
